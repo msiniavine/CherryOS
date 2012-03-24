48ba057bd49365595e811362dd14bd2cddb1e0c0
Maxim Siniavine <m.siniavine@gmail.com>
2012-03-24 10:11:31 -0400

    Fixed pte and pde definitions

--- include/mm.h
+++ include/mm.h
@@ -10,13 +10,11 @@ struct pte
 	u32     p:1,  // present
 		rw:1,  // read/write
 		us:1,  //user/supervisor
-		pwt:1, // page level write through
-		pcd:1, // page cache disabled
 		a:1,   // accesses bit, cpu sets whis if the page is accessed
 		d:1, // dirty, cpu sets this if the page was written to
 		reserved:7, // must be set to 0
 		addr:20; // physical address
-};
+} __attribute__((packed));
 
 // Page directory entry
 struct pde
@@ -27,8 +25,12 @@ struct pde
 		pwt:1,
 		pcd:1, 
 		a:1,
-		reserved:8, // must be 0
+		reserved:6, // must be 0
 		addr:20;  // address of the page table
-};
+} __attribute__((packed));
+
+// Initializes memory management
+// sets up identity paging for address range [start:end)
+void init_mm(u32 start, u32 end);
 
 #endif
